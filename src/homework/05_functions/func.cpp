b282608b3a88d93a518b128b13dc2907a872812d
alexshalar437 <oleksandr.shalar@g.austincc.edu>
2025-03-09 03:59:57 +0000

    HomeWork_5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,37 @@
 //add include statements
-
+#include "func.h"
+#include <iostream>
 //add function code here
+
+double get_gc_content(const std::string& dna) {
+    if (dna.empty()) return 0.0;
+
+    int gc_count = 0;
+    for (char nucleotide : dna) {
+        if (nucleotide == 'G' || nucleotide == 'C') {
+            gc_count++;
+        }
+    }
+    return static_cast<double>(gc_count) / dna.length();
+}
+
+std::string get_reverse_string(std::string dna) {
+    std::string reversed = "";
+    for (int i = dna.length() - 1; i >= 0; --i) {
+        reversed += dna[i];
+    }
+    return reversed;
+}
+
+std::string get_dna_complement(std::string dna) {
+    std::string reversed = get_reverse_string(dna);
+    for (char& nucleotide : reversed) {
+        switch (nucleotide) {
+            case 'A': nucleotide = 'T'; break;
+            case 'T': nucleotide = 'A'; break;
+            case 'C': nucleotide = 'G'; break;
+            case 'G': nucleotide = 'C'; break;
+        }
+    }
+    return reversed;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,31 @@
 
-int main() 
-{
+#include <iostream>
+#include "func.h"
 
-	return 0;
+int main() {
+    int choice;
+    std::string dna;
+
+    do {
+        std::cout << "Menu\n";
+        std::cout << "1- Get GC Content\n";
+        std::cout << "2- Get DNA Complement\n";
+        std::cout << "3- Exit\n";
+        std::cout << "Enter choice: ";
+        std::cin >> choice;
+
+        if (choice == 1) {
+            std::cout << "Enter DNA string: ";
+            std::cin >> dna;
+            std::cout << "GC Content: " << get_gc_content(dna) << std::endl;
+        } 
+        else if (choice == 2) {
+            std::cout << "Enter DNA string: ";
+            std::cin >> dna;
+            std::cout << "DNA Complement: " << get_dna_complement(dna) << std::endl;
+        }
+
+    } while (choice != 3);
+
+    return 0;
 }
